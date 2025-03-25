da5ae66b6d9410765494c96ac9e24d97b261885e
asharovatova <co.rin@mail.ru>
2025-03-25 20:08:30 +0300

    feat: implement getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -226,8 +226,14 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom;
+
+  return () => {
+    const res = count;
+    count += 1;
+    return res;
+  };
 }
 
 module.exports = {
